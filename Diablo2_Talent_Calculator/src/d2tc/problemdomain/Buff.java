a2c30105acc865f993a7d32f5004538319ba4afe
Jacque <jacquepointer1@gmail.com>
2022-08-29 10:46:12 -0600

    Debuff and Buff Class updated

--- Diablo2_Talent_Calculator/src/d2tc/problemdomain/Buff.java
+++ Diablo2_Talent_Calculator/src/d2tc/problemdomain/Buff.java
@@ -3,6 +3,13 @@ package d2tc.problemdomain;
 public class Buff {
 	private int buffValue;
 	private String buffName;
+	private String buffDescription;
+	
+	public Buff(String buffName, int buffValue, String buffDescription) {
+		this.buffDescription = buffDescription;
+		this.buffName = buffName;
+		this.buffValue = buffValue;
+	}
 	
 	public int getBuffValue() {
 		return buffValue;
@@ -15,6 +22,14 @@ public class Buff {
 	}
 	public void setBuffName(String buffName) {
 		this.buffName = buffName;
+	}
+
+	public String getBuffDescription() {
+		return buffDescription;
+	}
+
+	public void setBuffDescription(String buffDescription) {
+		this.buffDescription = buffDescription;
 	}
 	
 	
--- Diablo2_Talent_Calculator/src/d2tc/problemdomain/Debuff.java
+++ Diablo2_Talent_Calculator/src/d2tc/problemdomain/Debuff.java
@@ -4,4 +4,31 @@ public class Debuff {
 	private int debuffValue;
 	private String debuffName;
 	private String debuffDescription;
+	
+	public Debuff(String debuffName, int debuffValue, String debuffDescription) {
+		this.debuffDescription = debuffDescription;
+		this.debuffName = debuffName;
+		this.debuffValue = debuffValue;
+	}
+	
+	public int getDebuffValue() {
+		return debuffValue;
+	}
+	public void setDebuffValue(int debuffValue) {
+		this.debuffValue = debuffValue;
+	}
+	public String getDebuffName() {
+		return debuffName;
+	}
+	public void setDebuffName(String debuffName) {
+		this.debuffName = debuffName;
+	}
+	public String getDebuffDescription() {
+		return debuffDescription;
+	}
+	public void setDebuffDescription(String debuffDescription) {
+		this.debuffDescription = debuffDescription;
+	}
+	
+	
 }
